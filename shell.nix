dc02396ba44a3a92f819e52b46d2ecb4b698a639
Alessandro Candolini <alessandrocandolini@gmail.com>
2023-09-30 21:04:38 +0100

    Remove build tools

--- shell.nix
+++ shell.nix
@@ -12,7 +12,6 @@ let
 
   android-sdk = android-nixpkgs.sdk (sdkPkgs: with sdkPkgs; [
     cmdline-tools-latest
-    build-tools-30-0-3
     build-tools-33-0-1
     platform-tools
     platforms-android-33
