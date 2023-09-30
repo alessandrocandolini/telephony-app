b92d5df3e883e13cfef51a6453521f8f118d5a9d
Alessandro Candolini <alessandrocandolini@gmail.com>
2023-09-30 22:06:27 +0100

    try to add scan

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -15,3 +15,15 @@ dependencyResolutionManagement {
 
 rootProject.name = "telephony-app"
 include(":app")
+
+
+plugins {
+    `gradle-enterprise`
+}
+
+gradleEnterprise {
+    buildScan {
+        termsOfServiceUrl = "https://gradle.com/terms-of-service"
+        termsOfServiceAgree = "yes"
+    }
+}
